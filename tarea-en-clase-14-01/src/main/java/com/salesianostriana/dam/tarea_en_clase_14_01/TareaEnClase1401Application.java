88cd3a404293f98519f3b14d95a238ea3e89a102
Cristina Rus Morales <cristinarm2005@gmail.com>
2026-01-14 09:17:15 +0100

    proyecto creado

---
+++ tarea-en-clase-14-01/src/main/java/com/salesianostriana/dam/tarea_en_clase_14_01/TareaEnClase1401Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.tarea_en_clase_14_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaEnClase1401Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaEnClase1401Application.class, args);
+	}
+
+}
---
+++ tarea-en-clase-14-01/src/test/java/com/salesianostriana/dam/tarea_en_clase_14_01/TareaEnClase1401ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.tarea_en_clase_14_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaEnClase1401ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
